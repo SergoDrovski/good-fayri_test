854b351555b79f6c071653ad7992c8baec5d8854
sergiodrovski <sergiodrovski@gmail.com>
2023-05-19 18:48:00 +0300

    feat: change send api

--- frontend/next/pages/api/send.js
+++ frontend/next/pages/api/send.js
@@ -3,18 +3,18 @@ const chat_id_order = process.env.CHAT_ID_ORDER;
 
 
 export default async function handler(req, res) {
-    const isPhone = req.body.phone ?? req.body.phone !== "";
-    const isText = req.body.text ?? req.body.text !== "";
-    console.log("phone "+isPhone)
-    console.log("text "+isText)
-    if(isText || isPhone){
-        const chat_id = req.query.id ? `-${req.query.id}`: chat_id_order;
-        const URL = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&parse_mode=html`
-        try{
+    const isPhone = req.body?.phone !== undefined ?? req.body.phone !== "";
+    const isText = req.body?.text !== undefined ?? req.body.text !== "";
+    console.log("phone " + isPhone)
+    console.log("text " + isText)
+    try {
+        if (isText || isPhone) {
+            const chat_id = req.query.id ? `-${req.query.id}` : chat_id_order;
+            const URL = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&parse_mode=html`
             const today = new Date();
             const day = today.toLocaleDateString('en-US');
             const now = today.toLocaleTimeString('en-US');
-            let txt = `${now} ${day}`+ "%0A";
+            let txt = `${now} ${day}` + "%0A";
             for (let prop in req.body) {
                 txt += `<b>${prop}</b>: ${req.body[prop]}%0A`;
             }
@@ -26,20 +26,21 @@ export default async function handler(req, res) {
                 method: "post",
             })
             const json = await telegramRes.json();
-            if(!json.ok) {
+            if (!json.ok) {
                 throw new Error('fail request!')
             }
-            res.status(200).json({
-                data: { ...json },
+            return res.status(200).json({
+                data: {...json},
                 errors: null
             })
-
-        } catch (e) {
-            res.status(500).json({
-                data: null,
-                errors: e.message
-            })
+        } else {
+            return res.status(404).json({errors: 'Некоректные данные!'});
         }
+
+    } catch (e) {
+        return res.status(500).json({
+            data: null,
+            errors: e.message
+        })
     }
-    return res.status(404).json({errors: 'Некоректные данные!'});
 }
